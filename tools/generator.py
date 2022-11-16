392db3ca0f922a2ec6e0edbba27e4e87f66d0f79
Wafel <heavywolfpl@gmail.com>
2022-11-16 23:36:28 +0100

    Assign teachers function

--- tools/generator.py
+++ tools/generator.py
@@ -3,6 +3,7 @@ from tabulate import tabulate
 
 # Dataclasses
 from data.dataclasses import *
+from operator import attrgetter
 
 # Misc Tools
 from tools.tools import MiscTools
@@ -11,8 +12,6 @@ from sys import _getframe
 
 # Database Stuff
 from tools.tools import DatabaseTools
-import mysql.connector
-from mysql.connector import errorcode
 
 class Generator():
     def __init__(self):
@@ -46,6 +45,9 @@ class Generator():
             exit()
             # Restart.rerun() #TODO: ImportError: cannot import name 'Generator' from partially initialized module 'tools.Generator' (most likely due to a circular import)
 
+    def assign_headteachers(self):
+        pass # N/A for 1-3 years of school
+
     def assign_teachers(self):
         query = DatabaseTools.databaseQuery(self, f"SELECT klasa FROM klasy", Generator)
 
@@ -61,30 +63,99 @@ class Generator():
             print(classs)
         print(query)
 
-        query = DatabaseTools.databaseQuery(self, f"SELECT * FROM przedmioty", Generator)
-
+        query = DatabaseTools.databaseQuery(self, f"SELECT * FROM `przedmioty` ORDER BY `Nazwa` ASC", Generator)
         przedmioty = []
         for przedmiot in query:
             przedmioty.append(Subject(
                 name = przedmiot[0],
-                first_class_hr = int(przedmiot[1]),
-                second_class_hr = int(przedmiot[2]),
-                third_class_hr = int(przedmiot[3]),
+                teacher_preference = int(przedmiot[1]),
+                first_class_hr = int(przedmiot[2]),
+                second_class_hr = int(przedmiot[3]),
+                third_class_hr = int(przedmiot[4])
             ))
-
         print(przedmioty)
 
-        query = DatabaseTools.databaseQuery(self, f"SELECT * FROM nauczyciele", Generator)
-
+        query = DatabaseTools.databaseQuery(self, f"SELECT * FROM `nauczyciele` ORDER BY `Skrot` ASC", Generator)
         nauczyciele = []
         for nauczyciel in query:
             nauczyciele.append(Teacher(
                 short_name = nauczyciel[0],
                 name = nauczyciel[1],
                 subjects = nauczyciel[2],
+                headteacher = int(nauczyciel[3]),
+                hours = int(nauczyciel[4])
+            ))
+        nauczyciele.append(Teacher(name="", short_name="", subjects="", headteacher=0, hours=0)) 
+        # This is done to prevent teacher not being assigned to a subject, when he/she is headteacher, and subject is taught by headteacher
+        print(nauczyciele)
+
+        query = DatabaseTools.databaseQuery(self, f"SELECT * FROM `klasy` ORDER BY `Klasa` ASC", Generator)
+        klasy = []
+        for klasa in sorted(query):
+            klasy.append(Class(
+                name = klasa[0],
+                students = klasa[1],
+                headteacher = klasa[2]
             ))
 
-
+        print()
         
+        rows = []
+        teachers_not_found = []
+        for przedmiot in sorted(przedmioty, reverse = True, key=attrgetter('teacher_preference', 'name')): # Reverse = True - teacher_preference in descending order
+            for klasa in klasy:
+                query = DatabaseTools.databaseQuery(self, f"SELECT * from przydzieleni_nauczyciele WHERE (przedmiot = '{przedmiot.name}') AND (klasa = '{klasa.name}')", Generator) # klasy musi być w nawiasie, bruh?
+                if query:
+                    print(f"Nauczyciel przydzielony dla {klasa.name} | {przedmiot.name}:")
+                    print(f"\t{query}")
+                    print("------------------------")
+                else:
+                    for nauczyciel in sorted(nauczyciele, key=attrgetter('subjects', 'hours')):
+                        if nauczyciel.subjects == przedmiot.name:
+                            if (przedmiot.teacher_preference == 1) and (nauczyciel.name != klasa.headteacher):
+                                continue
 
-        # INSERT INTO `przydzieleni_nauczyciele` (`Przedmiot`, `Nauczyciel`, `Klasy`) VALUES ('Edukacja wczesnoszkolna', 'Paulina Cukierman', '1A');
+                            if klasa.name[0] == "1":
+                                nauczyciel.hours += przedmiot.first_class_hr
+                            elif klasa.name[0] == "2":
+                                nauczyciel.hours += przedmiot.second_class_hr
+                            elif klasa.name[0] == "3":
+                                nauczyciel.hours += przedmiot.third_class_hr
+                            if nauczyciel.hours > 18:
+                                LoggingTools.log(self, f"Nauczyciel {nauczyciel.short_name} ({przedmiot.name}) nie może zostać przydzielony do klasy {klasa.name} z powodu przekroczenia limitu godzin.", "debug")
+                                teachers_not_found.append(f"{klasa.name} | {przedmiot.name} | Brak wolnego etatu")
+                                pass
+                            
+                            query = DatabaseTools.databaseModify(self, f"INSERT INTO `przydzieleni_nauczyciele` (`Lp`, `Przedmiot`, `Nauczyciel`, `Klasa`) VALUES (0, '{przedmiot.name}', '{nauczyciel.name}', '{klasa.name}')", Generator)
+                            if query == 0:
+                                print(f"Nie udało się przydzielić nauczyciela {nauczyciel.name} do klasy {klasa.name} na przedmiot {przedmiot.name}.")
+                                LoggingTools.log(self, f"Nie udało się przydzielić nauczyciela {nauczyciel.name} do klasy {klasa.name} na przedmiot {przedmiot.name}.", "error", _getframe().f_lineno)
+
+                            query = DatabaseTools.databaseModify(self, f"UPDATE `nauczyciele` SET `Godziny` = '{nauczyciel.hours}' WHERE `Skrot` = '{nauczyciel.short_name}'", Generator)
+                            if query == 0:
+                                print(f"Nie udało się zaktualizować godzin nauczyciela {nauczyciel.name} ({nauczyciel.short_name}).")
+                                LoggingTools.log(self, f"Nie udało się zaktualizować godzin nauczyciela {nauczyciel.name} ({nauczyciel.short_name}).", "error", _getframe().f_lineno)
+
+                            print(f"Przydzielono nauczyciela dla {klasa.name} | {przedmiot.name}:")
+                            print(f"\t{nauczyciel.name} | Godziny: {nauczyciel.hours}")
+                            print("------------------------")
+                            rows.append([klasa.name, przedmiot.name, nauczyciel.name])
+                            break # break the "for nauczyciel in nauczyciele" loop
+
+                        else:
+                            pass # Executes if teacher doesn't teach the subject
+                    else:
+                        if przedmiot.teacher_preference == 1:
+                            teachers_not_found.append(f"{klasa.name} | {przedmiot.name} | Wychowawca nie może zostać nauczycielem przedmiotu")
+                        else:
+                            teachers_not_found.append(f"{klasa.name} | {przedmiot.name} | Brak nauczyciela przedmiotu")
+            else:
+                rows.append(["---------", "---------", "---------"] ) # Separating line between subjects
+                pass # Executes after the "for klasa in klasy" loop
+        else:
+            pass # Executes after the "for przedmiot in przedmioty" loop
+
+        rows.pop() # Removes the last separating line, as it somehow doubles
+        headers = ["Klasa", "Przedmiot", "Nauczyciel"]
+        print(tabulate(rows, headers, tablefmt="orgtbl", stralign="center"))
+        print("Nie znaleziono nauczycieli dla klas: \n\t{}".format(', \n\t'.join(teachers_not_found)))
